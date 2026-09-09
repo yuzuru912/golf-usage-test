c4499681d0620dbcd6184029a3de433f31c2d4e9
yuzuru912 <yzr2035@gmail.com>
2026-09-09 14:40:26 +0800

    Add files via upload

--- js/renderer.js
+++ js/renderer.js
@@ -107,7 +107,7 @@ function render() {
       ? '資料已更新 ' + pad(lastLoad.getHours()) + ':' + pad(lastLoad.getMinutes())
       : '';
   }
-   if ($$('#foot')) {
+  if ($$('#foot')) {
     $$('#foot').innerHTML =
       '可查詢範圍：' + MIN.replace(/-/g, '/') + " ～ " + MAX.replace(/-/g, '/') +
       '<br><small style="font-weight: normal; font-size: 12px;">Copyright© 2026 CSUN Charlotte Web Design</small>';
